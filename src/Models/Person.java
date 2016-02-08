bb5e5158ab848dd6383b97251a6682a4648a6401
ajarratt06 <ajarratt@cse.unl.edu>
2016-02-07 20:42:24 -0600

    Update Person.java

--- src/Models/Person.java
+++ src/Models/Person.java
@@ -3,8 +3,21 @@ package Models;
 /**
  * Person.java
  * Purpose      :   Models a person (referenced by customer)
- * Author       :   Jared Frenzel
+ * Author       :   Jared Frenzel, Abbie Jarratt, Emily Wagner
  * Created On   :   February 4, 2016
  */
 public class Person {
+  private String personCode;
+  private String fistName;
+  private String lastName;
+  private Address address;
+  private Arraylist<String> email;
+ 
+  public Person(String personCode, String firstName, String lastName, Address address, Arraylist<String> email){
+    this.personCode = personCode;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.email = email;
+  }
 }
