f4f239220daec23e6dea32ec4069cf730b9c9649
ajarratt06 <ajarratt@cse.unl.edu>
2016-02-07 20:35:16 -0600

    Update Address.java

--- src/Models/Address.java
+++ src/Models/Address.java
@@ -3,7 +3,7 @@ package Models;
 /**
  * Address.java
  * Purpose      :   Models an address (used by Customer and Person)
- * Author       :   Jared Frenzel
+ * Author       :   Jared Frenzel, Abbie Jarratt, Emily Wagner
  * Created On   :   February 4, 2016
  **/
 public class Address {
@@ -11,11 +11,13 @@ public class Address {
   private String city;
   private String state;
   private int zipcode;
+  private String country;
   
-  public Address(String street, String city, String state, int zipcode){
+  public Address(String street, String city, String state, int zipcode, String country){
     this.street = street;
     this.city = city;
     this.state = state;
     this.zipcode = zipcode;
+    this.country = country;
   }
 }
