b226eea0f0463b65ee9d546343873963bba388ff
Jared <Jared.frenzel.77@gmail.com>
2016-02-09 22:09:37 -0600

    Update Product.java

--- src/Models/Products/Product.java
+++ src/Models/Products/Product.java
@@ -6,5 +6,39 @@ package Models.Products;
  * Author       :   Jared Frenzel
  * Created On   :   February 4, 2016
  */
-public class Product {
+public abstract class Product {
+    private String productCode;
+    private String productName;
+
+    public String getProductCode() { return productCode; }
+    public String getProductName() { return productName; }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (productCode != null ? !productCode.equals(product.productCode) : product.productCode != null) return false;
+        return productName != null ? productName.equals(product.productName) : product.productName == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = productCode != null ? productCode.hashCode() : 0;
+        result = 31 * result + (productName != null ? productName.hashCode() : 0);
+        return result;
+    }
+
+
+    public void createFromJSON(String s) {
+
+    }
+
+    public void createFromCSV(String s) {
+
+    }
 }
